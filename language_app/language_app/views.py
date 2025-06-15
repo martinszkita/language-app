5d84709a1bb6cf65017ddf8eabab3011a8f35cfe
martinszkita <marpozniak1@gmail.com>
2025-06-15 11:18:37 +0200

    fix query error

--- language_app/language_app/views.py
+++ language_app/language_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import request
 from vocabulary.models import Language
 
-def home_page(reqest):
+def home_page(request):
     languages = Language.objects.all()
     return render(request, 'language_app/home.html', {'languages': languages})
     
--- language_app/vocabulary/views.py
+++ language_app/vocabulary/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.db import models
 from .models import Word, Language
 from .forms import WordForm
@@ -6,11 +6,21 @@ from django.contrib import messages
 from django.urls import resolve
 
 # Create your views here.
-def word_list(request):
-    language = resolve(request.path_info).kwargs.get('language')
-    print("chosen language:", language)
-    words = Word.objects.all
-    return render(request, 'vocabulary/word_list.html', {'language': language, 'words': words})
+def word_list(request, language):
+    """Display words for the selected language."""
+    if language is None:
+        language = resolve(request.path_info).kwargs.get('language')
+
+    language_obj = get_object_or_404(Language, name__iexact=language)
+    words = Word.objects.filter(language=language_obj)
+    return render(
+        request,
+        'vocabulary/word_list.html',
+        {
+            'language': language_obj.name,
+            'words': words,
+        },
+    )
 
 
 def vocabulary_home(request, language):
